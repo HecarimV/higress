86f895c15295540de0e57079ddd634298b1ad665
liuhaojie <liuhaojie2@harmonycloud.cn>
2025-04-03 17:34:18 +0800

    feat(ai-proxy): 添加对 Amazon Bedrock 的支持

---
+++ plugins/wasm-go/extensions/ai-proxy/provider/bedrock.go
@@ -0,0 +1,61 @@
+package provider
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/alibaba/higress/plugins/wasm-go/extensions/ai-proxy/util"
+	"github.com/alibaba/higress/plugins/wasm-go/pkg/wrapper"
+)
+
+const (
+	// converse路径 /model/{modelId}/converse
+	bedrockChatCompletionPath = "/model/%s/converse"
+	// converseStream路径 /model/{modelId}/converse-stream
+	bedrockStreamChatCompletionPath = "/model/%s/converse-stream"
+)
+
+type bedrockProviderInitializer struct {
+}
+
+func (b *bedrockProviderInitializer) ValidateConfig(config *ProviderConfig) error {
+	if len(config.awsAccessKey) == 0 || len(config.awsSecretKey) == 0 {
+		return errors.New("missing bedrock access authentication parameters")
+	}
+	return nil
+}
+
+func (b *bedrockProviderInitializer) DefaultCapabilities() map[string]string {
+	return map[string]string{
+		string(ApiNameChatCompletion): bedrockChatCompletionPath,
+	}
+}
+
+func (b *bedrockProviderInitializer) CreateProvider(config ProviderConfig) (Provider, error) {
+	config.setDefaultCapabilities(b.DefaultCapabilities())
+	return &bedrockProvider{
+		config:       config,
+		contextCache: createContextCache(&config),
+	}, nil
+}
+
+type bedrockProvider struct {
+	config       ProviderConfig
+	contextCache *contextCache
+}
+
+func (b *bedrockProvider) GetProviderType() string {
+	return providerTypeBedrock
+}
+
+func (b *bedrockProvider) OnRequestHeaders(ctx wrapper.HttpContext, apiName ApiName) error {
+	b.config.handleRequestHeaders(b, ctx, apiName)
+	return nil
+}
+
+func (b *bedrockProvider) TransformRequestHeaders(ctx wrapper.HttpContext, apiName ApiName, headers http.Header) {
+	util.OverwriteRequestAuthorizationHeader(headers, "")
+	if !b.config.IsOriginal() {
+		util.OverwriteRequestPathHeaderByCapability(headers, string(apiName), b.config.capabilities)
+	}
+}
